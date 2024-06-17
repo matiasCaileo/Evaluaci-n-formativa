b80e63b7f1e18dca4e781329ffded2eaec8a68c2
matiasCaileo <ma.caileo@duocuc.cl>
2024-06-17 18:37:57 -0400

    Create funciones.py

---
+++ PYTHON2.1/funciones.py
@@ -0,0 +1,30 @@
+empleados = []
+
+def registrar_empleado():
+    nombre = input("Nombre: ")
+    print("Escriba su cargo: ")
+    print("Ceo")
+    print("Desarrollador")
+    print("Analista de datos")
+    cargo = input("Cargo: ")
+    sueldo = float(input("Sueldo: "))
+    pobres={"Nombre": nombre, "Cargo": cargo, "Sueldo": sueldo, "Sueldo Neto": sueldo * 0.83}
+    empleados.append(pobres)
+    print("Empleado registrado!")
+
+def lista_empleados():
+    for empleado in empleados:
+        print(empleado)
+
+def imprimir_planilla():
+    empleados = lista_empleados()
+    
+    with open('PYTHON2.1/Archivo nuevo.txt', 'w', encoding='utf-8') as archivo:
+        for empleado in empleados:
+            nombre = empleado['Nombre']
+            cargo = empleado['Cargo']
+            sueldo_bruto = empleado['Sueldo']
+            sueldo_neto = empleado['Sueldo Neto']
+            archivo.write(f"{nombre} {cargo} {sueldo_bruto} {sueldo_neto}\n")
+    
+    print("Planilla de sueldos completa generada correctamente en planilla_sueldos_completa.txt")
